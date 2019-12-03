ab0f90d8f3ebe7ed652d38c417a9b9f7d9a1946a
Noble Amarteifio <rasgbxn@gmail.com>
2019-12-03 18:12:40 -0500

    Week 2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,44 +1,55 @@
 import React, { Component } from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap'
-
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+import { CAMPSITES } from '../shared/campsites';
+import Directory from './DirectoryComponent'
 
 class CampsiteInfo extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            campsite: CAMPSITES
+        };
 
     }
    
 
-
-
-    render() {
-        /**
-
-You can now delete the rest of the renderSelectedCampsite method from the Directory component. 
-Inside the CampsiteInfo component's render() method, inside the empty <div> 
-with the row class you set up earlier, call the renderCampsite method (remember to use this)
- and pass the campsite to it.
-Check your app in your browser. Make sure that when you click a campsite from the Directory, 
-you see a Card appear at the bottom with the campsite information. */
-        return(
-            <div className ="col-md-5 m-1">
-                if () {
-            return (
+    renderCampsite(campsite) {
+        
+        return (
+            <div className="col-md-5 m-1">
+                
                 <Card>
-                    <CardImg src = {campsite.image} alt ={campsite.name} />
+                    <CardImg src={campsite.image} alt={campsite.name} />
                     <CardBody>
                         <CardTitle>{campsite.name}</CardTitle>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
-               
-            );
-        }
-        return <div />
-            
             </div>
-        )
+        );
+
+
+    }
+    /*renderComments(comments){
+        if ()
+    }
+*/
+
+    render() {
+
+
+        if (this.props.campsite) {
+
+            return (
+                <div className="row">
+                    {this.renderCampsite(this.state.campsite)}
+                </div>
+            )
+        }
+        return <div></div>
+
+
+
     }
 
 }
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap'
+import CampsiteInfo from './CampsiteInfoComponent'
+
 
 class Directory extends Component {
     constructor(props) {
@@ -10,23 +12,13 @@ class Directory extends Component {
 
     }
     onCampsiteSelect(campsite){
+        
         this.setState({selectedCampsite: campsite});
+        
+        
     }
 
-    renderSelectedCampsite(campsite){
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg src = {campsite.image} alt ={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />
-    }
+
 
     render() {
         const directory = this.props.campsites.map(campsite => {
@@ -46,11 +38,7 @@ class Directory extends Component {
                     <div className = "row">
                         {directory}
                     </div>
-                    <div className = "row">
-                        <div className = "col-md-5 m-1">
-                            {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                        </div>
-                    </div>
+                    <CampsiteInfo />
                 </div>
 
        );
