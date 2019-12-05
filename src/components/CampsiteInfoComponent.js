1f5782c446723ea6cba11bcbbf3930161169b617
Noble Amarteifio <rasgbxn@gmail.com>
2019-12-05 05:05:21 -0500

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -51,10 +51,12 @@ class CampsiteInfo extends Component {
         if (this.props.campsite) {
 
             return (
-                <div className="row">
+                <div className="container">
+                    <div className="row">
                     
                     {this.renderCampsite(this.props.campsite)}
                     {this.renderComments(this.props.campsite.comments)}
+                </div>
                 </div>
             )
         }
