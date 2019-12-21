7d08304b6b59a2c9bf27982294265c0466eb4a68
Noble Amarteifio <rasgbxn@gmail.com>
2019-12-21 07:06:58 -0500

    React Animation Components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import { Loading } from './LoadingComponent'
+import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrls';
-
+import {FadeTransform} from 'react-animation-component'
 
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
@@ -14,6 +14,11 @@ function RenderCard({ item, isLoading, errMess }) {
     }
 
     return (
+        <FadeTransform
+        in
+        transformProps={{
+            exitTransform: 'scale(0.5) translateY(50%)'
+        }}>
         <Card>
             <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
@@ -21,6 +26,7 @@ function RenderCard({ item, isLoading, errMess }) {
                 <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
+    </FadeTransform>
     );
 }
 
